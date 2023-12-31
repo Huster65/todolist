918a2893c2048301207b9020a275d39694a2d121
Huster65 <hoangminhnhat2k2@gmail.com>
2024-01-01 01:25:21 +0700

    make it better

--- client/src/contexts/contants.js
+++ client/src/contexts/contants.js
@@ -1,2 +1,2 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'somedeployURL'
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'https://desolate-coast-01000-b0f7404eccf5.herokuapp.com/api' : 'somedeployURL'
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
